99be36e328b58c69b664c79f219a5a95b7b8b1f4
FuMiYu2011 <pmdung2011@gmail.com>
2021-06-30 12:52:51 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
+  const expenses = [
+    { id: 1, title: 'Food', amount: 301, date: new Date() },
+    { id: 2, title: 'Toilet Paper', amount: 300, date: new Date() },
+    { id: 3, title: 'Car Insurance', amount: 300, date: new Date() },
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +25,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
